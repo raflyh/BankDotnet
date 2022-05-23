bf0f8057e6304fbce3fa87c7197089525861306b
Rahadian Dwi Putra <dwiputrarahadian@gmail.com>
2022-05-24 03:17:50 +0700

    menambah saldo and cek saldo

---
+++ BalanceService/GraphQL/Mutation.cs
@@ -0,0 +1,29 @@
+﻿using Database.Models;
+using HotChocolate.AspNetCore.Authorization;
+
+namespace BalanceService.GraphQL
+{
+    public class Mutation
+    {
+        //[Authorize(Roles = new[] { "CUSTOMER" })]
+        public async Task<Balance> AddBalanceAsync(
+            BalanceInput input,
+            [Service] BankDotnetDbContext context)
+        {
+
+            // EF
+            var balance = new Balance
+            {
+                UserId = input.UserId,
+                AccountNumber = Guid.NewGuid().ToString() + input.UserId.ToString(),
+                TotalBalance = input.TotalBalance,
+                CreatedDate = DateTime.Now,
+            };
+
+            var ret = context.Balances.Add(balance);
+            await context.SaveChangesAsync();
+
+            return ret.Entity;
+        }
+    }
+}
