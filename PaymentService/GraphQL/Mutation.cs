6cbae8aaa04d93b278df7b41d4bd10acb5f59101
Rafly H <raflyh75@gmail.com>
2022-05-24 19:07:54 +0700

    adding configs

---
+++ PaymentService/GraphQL/Mutation.cs
@@ -0,0 +1,41 @@
+﻿using Database.Models;
+using HotChocolate.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace PaymentService.GraphQL
+{
+    public class Mutation
+    {
+        [Authorize(Roles = new[] { "CUSTOMER" })]
+        public async Task<TransactionStatus> PayTravikaAsync(
+            BillPayment input,
+            [Service] ClaimsPrincipal claimsPrincipal,
+            [Service] BankDotnetDbContext context)
+        {
+            var userName = claimsPrincipal.Identity.Name;
+            var customer = context.Users.Where(o => o.Username == userName).FirstOrDefault();
+            var customerBalance = context.Balances.Where(o => o.UserId == customer.Id).OrderBy(o => o.Id).LastOrDefault();
+            var customerCredit = context.Credits.Where(o => o.UserId == customer.Id).OrderBy(o => o.Id).LastOrDefault();
+
+            var newTransaction = new Transaction
+            {
+                CreditId = customerCredit.Id,
+                BalanceId = customerBalance.Id,
+                Total = 0,
+                TransactionDate = DateTime.Now,
+                Description = "Payment for Travika",
+                CreatedDate = DateTime.Now
+            };
+            var bill = context.Bills.Where(o => o.PaymentStatus == "Unpaid").FirstOrDefault();
+
+            var newBill = new Bill
+            {
+                TransactionId = newTransaction.Id,
+                VirtualAccount = input.VirtualAccount,
+                TotalBill = (double)input.TotalBill,
+                PaymentStatus = "Paid",
+                Type = input.Type
+            };
+        }
+    }
+}
---
+++ PaymentService/GraphQL/Query.cs
@@ -0,0 +1,31 @@
+﻿using Database.Models;
+using HotChocolate.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace PaymentService.GraphQL
+{
+    public class Query
+    {
+        [Authorize]
+        public IQueryable<Bill> GetBills(ClaimsPrincipal claimsPrincipal, [Service] BankDotnetDbContext context)
+        {
+            var userName = claimsPrincipal.Identity.Name;
+
+            var userRole = claimsPrincipal.Claims.Where(o => o.Type == ClaimTypes.Role).FirstOrDefault();
+            var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
+            if (user != null)
+            {
+                if (userRole != null)
+                {
+                    if (userRole.Value == "MANAGER")
+                    {
+                        return context.Bills.AsQueryable();
+                    }
+                }
+                var orders = context.Bills.Where(a => a.TransactionId == user.Id);
+                return orders.AsQueryable();
+            }
+            return new List<Bill>().AsQueryable();
+        }
+    }
+}
