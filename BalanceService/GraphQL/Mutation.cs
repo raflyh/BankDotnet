6f49da88fccf208e143a50ae1d721ffb42827f52
Rahadian Dwi Putra <dwiputrarahadian@gmail.com>
2022-05-25 18:11:17 +0700

    add transaction in redeem code

--- BalanceService/GraphQL/Mutation.cs
+++ BalanceService/GraphQL/Mutation.cs
@@ -39,7 +39,7 @@ namespace BalanceService.GraphQL
             };
         }
         [Authorize(Roles = new[] { "CUSTOMER SERVICE" })]
-        public async Task<TransferOutput> AddTransferAsync(
+        public async Task<TransferOutput> AddTransferWithCSAsync(
             TransferBalance input,
             [Service] BankDotnetDbContext context)
         {
@@ -110,11 +110,86 @@ namespace BalanceService.GraphQL
             }
         }
         [Authorize(Roles = new[] { "NASABAH" })]
+        public async Task<TransferOutput> AddTransferAsync(
+            TransferBalance input,
+            [Service] BankDotnetDbContext context,ClaimsPrincipal claimsPrincipal)
+        {
+            var userName = claimsPrincipal.Identity.Name;
+            var recipient = context.Balances.Where(x => x.AccountNumber == input.RecipientAccountNumber).FirstOrDefault();
+            var userRecipient = context.Users.Where(o => o.Id == recipient.UserId).FirstOrDefault();
+            var userSender = context.Users.Where(o => o.Username == userName).FirstOrDefault();
+            var sender = context.Balances.Where(s => s.UserId == userSender.Id).FirstOrDefault();
+
+            if (sender == null)
+                return new TransferOutput
+                {
+                    TransactionDate = DateTime.Now.ToString(),
+                    Message = "Penerima Tidak Ditemukan"
+                };
+            if (recipient == null)
+                return new TransferOutput
+                {
+                    TransactionDate = DateTime.Now.ToString(),
+                    Message = "Pengirim Tidak Ditemukan"
+                };
+            if (recipient != null && sender != null)
+            {
+                if (sender.TotalBalance > input.Total)
+                {
+                    var transaksi = new Transaction
+                    {
+                        RecipientBalanceId = recipient.Id,
+                        SenderBalanceId = sender.Id,
+                        Total = input.Total,
+                        TransactionDate = DateTime.Now,
+                        Description = input.Description
+                    };
+                    context.Transactions.Add(transaksi);
+
+                    sender.TotalBalance = (sender.TotalBalance - input.Total);
+                    context.Balances.Update(sender);
+
+                    recipient.TotalBalance = (recipient.TotalBalance + input.Total);
+                    context.Balances.Update(recipient);
+                    await context.SaveChangesAsync();
+
+                    return new TransferOutput
+                    {
+                        TransactionDate = DateTime.Now.ToString(),
+                        Message = "Transfer Berhasil",
+                        SenderAccount = $"{userSender.FullName}-{sender.AccountNumber}",
+                        RecipientAccount = $"{userRecipient.FullName}-{recipient.AccountNumber}",
+                        Description = input.Description,
+                    };
+                }
+                else
+                {
+                    return new TransferOutput
+                    {
+                        TransactionDate = DateTime.Now.ToString(),
+                        Message = "Saldo Tidak Mencukupi"
+                    };
+                }
+            }
+            else
+            {
+                return new TransferOutput
+                {
+                    TransactionDate = DateTime.Now.ToString(),
+                    Message = "Error"
+                };
+            }
+        }
+        [Authorize(Roles = new[] { "NASABAH" })]
         public async Task<TopupOutput> AddRedeemCodeAsync(
             TopupOpo input,
             ClaimsPrincipal claimsPrincipal,
             [Service] BankDotnetDbContext context, [Service] IOptions<KafkaSettings> settings)
         {
+            var userName = claimsPrincipal.Identity.Name;
+            var user = context.Users.Where(s=>s.Username == userName).FirstOrDefault();
+            var balance = context.Balances.Where(s=>s.UserId == user.Id).FirstOrDefault();
+            //create code generator
             const string src = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             int length = 16;
             var sb = new StringBuilder();
@@ -124,15 +199,15 @@ namespace BalanceService.GraphQL
                 var c = src[rdm.Next(0, src.Length)];
                 sb.Append(c);
             }
-            Console.WriteLine(sb.ToString());
+            //input
             input.Code = sb.ToString();
             input.Amount = input.Amount;
-
+            //send kafka
             var dts = DateTime.Now.ToString();
-            var key = "TopupOPO-" + dts;
+            var key = "RedeemCOde-" + dts;
             var val = JObject.FromObject(input).ToString(Formatting.None);/*JsonConvert.SerializeObject(input);*/
             var result = await KafkaHelper.SendMessage(settings.Value, "Latihan4", key, val);
-
+            
             TopupOutput resp = new TopupOutput
             {
                 TransactionDate = dts,
@@ -140,8 +215,18 @@ namespace BalanceService.GraphQL
             };
             if (!result)
                 resp.Message = "Failed to submit data";
+            //add transaksi
+            var transaksi = new Transaction
+            {
+                SenderBalanceId = balance.Id,
+                Total = input.Amount,
+                Description = $"Create Redeem Code - {user.FullName}",
+                TransactionDate = DateTime.Now,
+            };
+            context.Transactions.Add(transaksi); 
+            context.SaveChanges();
+
             return await Task.FromResult(resp);
-            
         }
     }
 }
